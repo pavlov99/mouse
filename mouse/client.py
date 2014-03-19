6140f81eb143d3131dc0fcf753b854080c7b94b3
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-03-19 10:44:17 +0000

    add missing methods to client based on settings

--- mouse/client.py
+++ mouse/client.py
@@ -3,6 +3,9 @@ from requests.auth import HTTPBasicAuth
 import logging
 import requests
 
+from .settings import REQUEST_MAP
+from .utils import get_string_kwargs, FrozenDict, get_partly_formated_string
+
 logger = logging.getLogger(__name__)
 
 
@@ -10,18 +13,24 @@ class Client(object):
 
     """ Api client implementation."""
 
-    BASE_URL = "https://cheddargetter.com/xml/{path}/productCode/" +\
-        "{product_code}"
+    BASE_URL = "https://cheddargetter.com/xml"
+    REQUEST_MAP = FrozenDict([
+        (
+            method["title"].replace(" a ", " ").replace(" ", "_").lower(),
+            method
+        ) for method in REQUEST_MAP
+    ])
 
     def __init__(self, username, password, product_code):
         self.username = username
         self.password = password
         self.product_code = product_code
-        self.base_url = self.BASE_URL.format(product_code=product_code)
 
-    def request(self, path, params, method=None):
-        method = method or "GET"
-        url = self.base_url.format(path=path)
+    def request(self, method, path, **params):
+        # TODO: exception handler
+        url = self.BASE_URL + path
+        logger.debug("Call {}: {} with params {}".format(method, url, params))
+        print("Call {}: {} with params {}".format(method, url, params))
         response = requests.request(
             method,
             url,
@@ -29,3 +38,25 @@ class Client(object):
             auth=HTTPBasicAuth(self.username, self.password)
         )
         return response
+
+    def __getattr__(self, method_name):
+        if method_name in self.REQUEST_MAP:
+            method = self.REQUEST_MAP[method_name]
+            path = get_partly_formated_string(
+                method["path"], {"product_code": self.product_code})
+            path_template = path.format(**{
+                x: "{" + str(i) + "}"
+                for i, x in enumerate(get_string_kwargs(path))
+            })
+
+            f = lambda *args, **kwargs: self.request(
+                method["request_method"],
+                path_template.format(*args),
+                **kwargs
+            )
+            f.__name__ = method_name
+            f.__doc__ = get_partly_formated_string(
+                method["doc"], {"product_code": self.product_code})
+            return f
+        else:
+            return super(Client, self).__getattr__(method_name)
